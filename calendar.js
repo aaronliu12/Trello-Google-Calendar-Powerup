1f92f026a74750ad70cffb47a8a8e834983208fd
Aaron <aaronliu12@gmail.com>
2016-07-16 17:47:06 +0800

    calendar.js

--- calendar.js
+++ calendar.js
@@ -12,7 +12,7 @@ TrelloPowerUp.initialize({
 						//var board = t.board('name');
 						t.board('name').then(function(board) {
 							localStorage.setItem("boardname", board.name);
-							console.log(board.name);
+							console.log(board.cards);
 						});
 						t.overlay({url: "./connect.html"
 						})
