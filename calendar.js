04f897e1e26845691efbd7b8f03708f4864a445d
Aaron <aaronliu12@gmail.com>
2016-07-17 12:49:06 +0800

    calendar.js

--- calendar.js
+++ calendar.js
@@ -1,5 +1,5 @@
 TrelloPowerUp.initialize({
-			'board-buttons': function(t, board) {
+			'board-buttons': function(t, options) {
 				
 				return [
 				{
@@ -8,11 +8,11 @@ TrelloPowerUp.initialize({
 					// Pass a URL instead of a callback to make a simple link
 
 					//url: 'https://calendar.google.com/calendar/b/render?aaronliu12@gmail.com#main_7',
-					callback: function(t, board) {
+					callback: function(t, options) {
 						//var board = t.board('name');
-						t.board('url').then(function(board) {
-							localStorage.setItem("boardname", board);
-							console.log(board);
+						t.options('url').then(function(options) {
+							localStorage.setItem("boardname", options);
+							console.log(options);
 						});
 
 						
