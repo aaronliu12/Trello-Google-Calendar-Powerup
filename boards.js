f4ec4a13383cd54f77d4b3ad58d4d72395c2ab8a
Aaron <aaronliu12@gmail.com>
2016-07-17 14:13:27 +0800

    first commit

---
+++ boards.js
@@ -0,0 +1,45 @@
+
+    var loadedBoards = function(boards) {
+      $.each(boards, function(index, value) {
+        $('#boards')
+          .append($("<option></option>")
+          .attr("value",value.id)
+          .text(value.name)); 
+      });
+    };
+    var loadBoards = function() {
+      //Get the users boards
+      Trello.get(
+        '/members/me/boards/',
+        loadedBoards,
+        function() { console.log("Failed to load boards"); }
+      );
+    };
+   
+    console.log(loadedBoards);
+
+    
+    var loadedCards = function(cards) {
+
+
+      
+      Trello.get(
+        '/boards/' + boardId + '/cards',
+        loadedCards,
+        function() { console.log("Failed to load labels"); }
+      );
+    });
+    
+
+    
+    
+    Trello.authorize({
+      type: "popup",
+      name: "Trello dashboard",
+      scope: {
+        read: true,
+        write: false },
+      expiration: "never",
+      success: loadBoards,
+      error: function() { console.log("Failed authentication"); }
+    });
