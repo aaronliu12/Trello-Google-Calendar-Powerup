cd7b78f1f3c9a72eb33a0336d88bb39b56e0cf49
Aaron <aaronliu12@gmail.com>
2016-07-17 14:21:27 +0800

    calendar.js

--- calendar.js
+++ calendar.js
@@ -1,5 +1,48 @@
-var a = document.getElementsByTagName("h2");
-console.log(a);
+
+    var loadedBoards = function(boards) {
+      $.each(boards, function(index, value) {
+        $('#boards')
+          .append($("<option></option>")
+          .attr("value",value.id)
+          .text(value.name)); 
+      });
+    };
+    var loadBoards = function() {
+      //Get the users boards
+      Trello.get(
+        '/members/me/boards/',
+        loadedBoards,
+        function() { console.log("Failed to load boards"); }
+      );
+    };
+   
+
+    
+    var loadedCards = function(cards) {
+
+
+      
+      Trello.get(
+        '/boards/' + boardId + '/cards',
+        loadedCards,
+        function() { console.log("Failed to load labels"); }
+      );
+    };
+    
+    console.log(loadedCards);
+
+    
+    
+    Trello.authorize({
+      type: "popup",
+      name: "Trello dashboard",
+      scope: {
+        read: true,
+        write: false },
+      expiration: "never",
+      success: loadBoards,
+      error: function() { console.log("Failed authentication"); }
+    });
 
 TrelloPowerUp.initialize({
 			'board-buttons': function(t, board) {
